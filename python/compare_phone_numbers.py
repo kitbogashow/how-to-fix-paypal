637c57666f29741f0b0d4d8325abd805fe8e91a1
Advistane <advistane@gmail.com>
2022-12-22 11:25:21 -0600

    Added additional checking method

--- python/compare_phone_numbers.py
+++ python/compare_phone_numbers.py
@@ -9,20 +9,36 @@ official_paypal_numbers = [
     "4029352050",
     "8889148072"
 ]
-lines = open(os.path.join(os.getcwd(), '..', 'invoices.txt'), encoding='utf-8')
 
-for index, line in enumerate(lines):  # Iterate through each line
+# Doesn't need to scan whole text to find phone numbers,
+# but could potentially break due to different phone number formats
+def check_performant(line_to_check):
     scam = False  # Assume an email isn't a scam initially
     line_stripped = re.sub(r'[^a-zA-Z0-9_+ ]', '',
-                           line)  # Remove all non-alphanumeric characters (except for _, +, and spaces)
+                           line_to_check)  # Remove all non-alphanumeric characters (except for _, +, and spaces)
     line_stripped = line_stripped.replace("+1", "")  # Remove country code indicator
 
     for match in re.finditer(r"\(?\b[2-9][0-9]{2}\)?[-. ]?[2-9][0-9]{2}[-. ]?[0-9]{4}\b",
                              line_stripped):  # Find phone numbers in the stripped text
         if match not in official_paypal_numbers:  # If the number is not in the official PayPal numbers, it's a scam
-            scam = True
-            break
+            return True
+
+    return False
+
+
+# Checks all numbers to see if an official Paypal number is included
+def check(line_to_check):
+    line_stripped = re.sub(r'[^0-9]', '', line_to_check)  # Extract only numbers
+    for number in official_paypal_numbers:
+        if number not in line_stripped:
+            return True
+
+
+lines = open(os.path.join(os.getcwd(), '..', 'invoices.txt'), encoding='utf-8')
+
+for index, line in enumerate(lines):  # Iterate through each line
 
     print(line)
-    print("Scam?", str(scam))
+    print("Scam (performant)?", str(check_performant(line)))
+    print("Scam?", str(check(line)))
     print("----------")
