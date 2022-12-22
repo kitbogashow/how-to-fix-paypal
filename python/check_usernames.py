a243dbe611d62809815f19e357d78d867eae9b48
Gamer1120 <Gamer1120@github.com>
2022-12-22 19:26:37 +0100

    Check usernames

---
+++ python/check_usernames.py
@@ -0,0 +1,6 @@
+recipient = 'PayPal User' # Insert recipient here
+
+if recipient == 'PayPal User':
+    print("Whoa, it looks like you're doing something sketchy there! Please change your username!")
+else:
+    print('Yeah your username seems legit, sending the message.')
