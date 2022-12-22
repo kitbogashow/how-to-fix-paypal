cef1aff67d6e2655526182a8950ec8f2409e207e
datatags <datatags@users.noreply.github.com>
2022-12-22 10:28:55 -0800

    Use better practices for Java regex example

--- java/src/main/java/FixPaypalRegex.java
+++ java/src/main/java/FixPaypalRegex.java
@@ -1,26 +1,17 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
 public class FixPaypalRegex {
-
+    private static final Pattern PATTERN = Pattern.compile("[0-9]{3,}|call|contact|\\\\+1");
     public static void main(String[] args) {
-        BufferedReader reader;
-
-        try {
-            reader = new BufferedReader(new FileReader("invoices.txt"));
-            String line = reader.readLine();
-
-            while (line != null) {
-                if (Pattern.compile("([0-9]{3,}|call|contact|\\\\+1)").matcher(line).find()){
+        try (BufferedReader reader = new BufferedReader(new FileReader("invoices.txt"))) {
+            reader.lines().forEach(line -> {
+                if (PATTERN.matcher(line).find()){
                     System.out.println("ඞ sus thing found: " + line);
                 }
-                line = reader.readLine();
-            }
-
-            reader.close();
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
